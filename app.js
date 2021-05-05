f7fbc08e06a08d484ad37dcfae7aaf0c845312bf
akyravish <info.akshayravish@gmail.com>
2021-05-05 12:31:51 +0530

    adding codes for the loading screen

---
+++ app.js
@@ -0,0 +1,24 @@
+// jshint esversion:6
+
+const bg = document.querySelector('.bg');
+const loadingText = document.querySelector('.loading-text');
+
+let load = 0;
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+const loading = () => {
+	load++;
+
+	if (load > 99) {
+		clearInterval(int);
+	}
+
+	loadingText.innerHTML = `${load}%`;
+	loadingText.style.opacity = `${scale(load, 0, 100, 1, 0)}`;
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+let int = setInterval(loading, 20);
